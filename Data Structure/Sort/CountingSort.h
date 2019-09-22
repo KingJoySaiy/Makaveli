e1b093ffb3323317597f1990b9c628973cfaf166
KingJoySaiy <kingjoysaiy@gmail.com>
2019-09-22 15:16:04 +0800

    finish C-file

--- Data Structure/Sort/CountingSort.h
+++ Data Structure/Sort/CountingSort.h
@@ -2,6 +2,7 @@
 #define INC_233_COUNTINGSORT_H
 
 #include <climits>
+#include <algorithm>
 
 namespace Counting {
 
@@ -27,7 +28,38 @@ namespace Counting {
 		delete b;
     }
     
-    template<class T> void Bucket(T *a, int n) {	//Bucket Sort
+    const int bucketNum = 10;	//define 10 buckets
+    
+    template<class T> void BucketArray(T *a, int n) {	//Bucket Sort + Bubblle Sort(array)
+    	
+    	int base = INT_MAX, back = INT_MIN, id = 0;
+		for (int i = 0; i < n; i++) {	//range [base, back]
+			if (a[i] < base) base = a[i];
+			if (a[i] > back) back = a[i];
+		}
+		T **p = new int*[bucketNum];	//initialize buckets
+		for (int i = 0; i < bucketNum; i++) {
+			p[i] = new int[n];
+		}
+		int ct[bucketNum] = {0}, range = (back - base + 1) / bucketNum + 1;	//count & range of each bucket
+		
+		for (int i = 0; i < n; i++) {
+			int tmp = (a[i] - base) / range;
+			p[tmp][ct[tmp]++] = a[i];
+		}
+		for (int k = 0; k < bucketNum; k++) {	//sort every buckey by BubbleSort
+			for (int i = 0; i < ct[k]; i++) {
+				for (int j = i; j > 0; j--) {
+					if (p[k][j - 1] > p[k][j]) std::swap(p[k][j - 1], p[k][j]);
+				}
+			}
+			for (int i = 0; i < ct[k]; i++) {
+				a[id++] = p[k][i];
+			}
+		}
+	}
+	
+	template<class T> void BucketList(T *a, int n) {	//Bucket Sort + Straight Insertion Sort(list)
     	
     	
 	}

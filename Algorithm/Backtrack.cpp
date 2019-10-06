72020d19231ee33bbf29c1a5569033441489f1cb
KingJoySaiy <kingjoysaiy@gmail.com>
2019-10-06 20:18:07 +0800

    finish branch & bound

--- Algorithm/Backtrack.cpp
+++ Algorithm/Backtrack.cpp
@@ -78,33 +78,44 @@ void nQueenTest() {	//test function
 
 
 /******** 0-1 package ********/
-int dfs3(int w[], int v[], int n, int left, bool a[], int id) {
+int selectPacId[maxn] = {}, pacLen = 0, maxValue = -inf;
+void dfs3(int w[], int v[], int n, int left, bool a[], int id) {	//enumerate every condition & update (select[], maxValue)
 	
 	if (id == n) {
-		int res = 0;
+		int sum = 0;
 		for (int i = 0; i < n; i++) {
-			if (a[i]) res += v[i];
+			if (a[i]) sum += v[i];
 		}
-		return res;
+		if (sum > maxValue) {
+			maxValue = sum;
+			pacLen = 0;
+			for (int i = 0; i < n; i++) {
+				if (a[i]) selectPacId[pacLen++] = i;
+			}
+		}
+		return;
 	}
-	int res = -inf;
 	if (left >= w[id]) {	//try to add id when it is possible
 		a[id] = true;
-		res = max(res, dfs3(w, v, n, left - w[id], a, id + 1));
+		dfs3(w, v, n, left - w[id], a, id + 1);
 	}
-	a[id] = false;	//dismiss id
-	return max(res, dfs3(w, v, n, left, a, id + 1));
+	a[id] = false;
+	dfs3(w, v, n, left, a, id + 1);	//dismiss id
 }
-int package(int w[], int v[], int n, int all) {
+void package(int w[], int v[], int n, int all) {
 	
 	bool a[maxn] = {};
-	return dfs3(w, v, n, all, a, 0);
+	dfs3(w, v, n, all, a, 0);
+	cout << "max value of the 0-1 package: " << maxValue << "\nselected id of the packages: ";
+	for (int i = 0; i < pacLen; i++) {
+		cout << selectPacId[i] << " \n"[i == pacLen - 1];
+	}
 }
 void packageTest() {	//test function
 	
 	int w[] = {2, 1, 3, 2}, v[] = {3, 2, 4, 2};
 	int n = 4, all = 5;
-	cout << "max value of the 0-1 package: " << package(w, v, n, all) << endl;
+	package(w, v, n, all);
 }
 
 
@@ -150,38 +161,42 @@ void nColoringTest() {	//test function
 
 
 /******** loading problem 2 ********/
-bool loadOK;	//mark whether loading is finished
-void dfs5(int n, int sum, int sum2, int w[], bool select[], int id) {
+int bestSum1 = -inf;	//maximum sum1 satisfying sum1 <= c1
+bool selectLoad[maxn] = {};	//mark 1st ship whether select the package
+void dfs5(int n, int sum, int w[], bool a[], int id) {
 	
-	if (!loadOK and id == n) {
+	if (id == n) {
 		int ct = 0;
 		for (int i = 0; i < n; i++) {
-			if (!select[i]) ct += w[i];
+			if (a[i]) ct += w[i];
 		}	
-		if (ct <= sum2) loadOK = true;	//if sum2 could load other packages then finished
+		if (ct > bestSum1) {	//if 1st ship could load more, then update result
+			for (int i = 0; i < n; i++) {
+				selectLoad[i] = a[i];
+			}
+			bestSum1 = ct;
+		}
 		return;
 	}
-	if (!loadOK and sum >= w[id]) {	//try to load id to 1st ship
-		select[id] = true;
-		dfs5(n, sum - w[id], sum2, w, select, id + 1);
+	if (sum >= w[id]) {	//try to load id to 1st ship
+		a[id] = true;
+		dfs5(n, sum - w[id], w, a, id + 1);
 	}
-	if (!loadOK) {	//dismiss id(load id to 2nd ship)
-		select[id] = false;
-		dfs5(n, sum, sum2, w, select, id + 1);
-	}
+	a[id] = false;		//dismiss id
+	dfs5(n, sum, w, a, id + 1);
 }
-void loading2(int c1, int c2, int n, int w[]) {	//load n package to 2 ships(maximum weight of them is c1 & c2)
+void loading2(int c1, int c2, int n, int w[]) {	//load n package to 2 ships(maximum weights of them are c1 & c2)
 	
-	loadOK = false;
-	bool select[maxn] = {};
-	dfs5(n, c1, c2, w, select, 0);
+	bool a[maxn] = {};	//temporary array
+	dfs5(n, c1, w, a, 0);
+	
 	int sum1 = 0, sum2 = 0;
 	for (int i = 0; i < n; i++) {
-		select[i] ? (sum1 += w[i]) : (sum2 += w[i]);
+		selectLoad[i] ? (sum1 += w[i]) : (sum2 += w[i]);
 	}
 	printf("load-weight of the 2 ships: %d, %d\nship number of each package: ", sum1, sum2);
 	for (int i = 0; i < n; i++) {
-		printf("%d%c", select[i] ? 1 : 2, i == n - 1 ? '\n' : ' ');
+		printf("%d%c", selectLoad[i] ? 1 : 2, " \n"[i == n - 1]);
 	}
 } 
 void loading2Test() {	//test function
--- Algorithm/BranchAndBound.cpp
+++ Algorithm/BranchAndBound.cpp
@@ -51,10 +51,121 @@ void DijkstraTest() {	//test function
 }
 
 
+/******** loading problem 2 ********/
+struct loadNode {	//binary tree
+	loadNode *parent;
+	int id, sum;	//current height & sum
+	bool ok;	//whether select id
+	loadNode(loadNode *parent, int id, int sum, bool ok) : parent(parent), id(id), sum(sum), ok(ok) {}
+};
+void loading2(int c1, int c2, int n, int w[]) {	//load n package to 2 ships(maximum weights of them are c1 & c2)
+	
+	loadNode *p[1 << (n + 1)] = {new loadNode(nullptr, -1, 0, false)};	//temporary pointer array 
+	queue<loadNode*> que;	//initialize the queue
+	que.push(p[0]);
+	bool select[maxn] = {};
+	int sum1 = 0, sum2 = 0, bestSum1 = 0, ct = 1;
+	
+	while (!que.empty()) {
+		loadNode *t = que.front(); que.pop();	//get the front of queue & pop it
+		if (t->id != n - 1) {
+			if (t->sum + w[t->id + 1] <= c1) {	//try to load id to 1st ship
+				p[ct++] = new loadNode(t, t->id + 1, t->sum + w[t->id + 1], true);
+				que.push(p[ct - 1]);
+			}
+			p[ct++] = new loadNode(t, t->id + 1, t->sum, false);	//dismiss id
+			que.push(p[ct - 1]);
+		} else if (t->sum > bestSum1) {	//if 1st ship could load more, then update result
+				bestSum1 = t->sum;
+				while (t->id != -1) {
+					select[t->id] = t->ok;
+					t = t->parent;
+				}
+		}
+	}
+	for (int i = 0; i < ct; i++) {	//free all pointer safely 
+		delete p[i];
+	}
+	for (int i = 0; i < n; i++) {
+		select[i] ? (sum1 += w[i]) : (sum2 += w[i]);
+	}
+	printf("load-weight of the 2 ships: %d, %d\nship number of each package: ", sum1, sum2);
+	for (int i = 0; i < n; i++) {
+		printf("%d%c", select[i] ? 1 : 2, " \n"[i == n - 1]);
+	}
+} 
+void loading2Test() {	//test function
+	
+	int c1 = 55, c2 = 35, n = 4, w[] = {20, 15, 30, 20};
+	loading2(c1, c2, n, w);
+}
+
+
+/******** 0-1 package ********/
+int weight[maxn], value[maxn];
+
+struct pacNode {	//binary tree
+	pacNode *parent;
+	int id, sumW, sumV;		//current height, sumWeight & sumValue
+	bool ok;	//whether select id
+	pacNode(pacNode *parent, int id, int sumW, int sumV, bool ok) : parent(parent), id(id), sumW(sumW), sumV(sumV), ok(ok) {}
+//	bool operator < (const pacNode &t) const {		//overload operator '<' (needed when use priority_queue) 
+//		return value[id] * weight[t.id] < value[t.id] * weight[id];
+//	}
+};
+void package(int n, int all) {
+	
+	int select[maxn] = {}, len = 0, maxValue = -inf, ct = 1;
+	pacNode *p[1 << (n + 1)] = {new pacNode(nullptr, -1, 0, 0, false)};	//temporary pointer array 
+//	priority_queue<pacNode*> que;	//my perspective: not necessary to use priority_queue, because all of the 2^(n+1)-1 pacNodes must be used
+	queue<pacNode*> que;	//initialize the queue
+	que.push(p[0]);
+	
+	while (!que.empty()) {
+		pacNode *t = que.front(); que.pop();	//get the front of queue & pop it
+		if (t->id != n - 1) {
+			if (t->sumW + weight[t->id + 1] <= all) {	//try to select id
+				p[ct++] = new pacNode(t, t->id + 1, t->sumW + weight[t->id + 1], t->sumV + value[t->id + 1], true);
+				que.push(p[ct - 1]);
+			}
+			p[ct++] = new pacNode(t, t->id + 1, t->sumW, t->sumV, false);	//dismiss id
+			que.push(p[ct - 1]);
+		} else if (t->sumV > maxValue) {	//if 1st ship could load more, then update result
+				maxValue = t->sumV;
+				len = 0;
+				while (t->id != -1) {
+					if (t->ok) select[len++] = t->id;
+					t = t->parent;
+				}
+		}
+	}
+	for (int i = 0; i < ct; i++) {	//free all pointer safely 
+		delete p[i];
+	}
+	cout << "max value of the 0-1 package: " << maxValue << "\nselected id of the packages: ";
+	for (int i = len - 1; ~i; i--) {
+		cout << select[i] << " \n"[!i];
+	}
+}
+void packageTest() {	//test function
+	
+	int w[] = {2, 1, 3, 2}, v[] = {3, 2, 4, 2};
+	int n = 4, all = 5;
+	for (int i = 0; i < n; i++) {
+		weight[i] = w[i];
+		value[i] = v[i];
+	}
+	package(n, all);
+}
+
+
 /******** main ********/
 int main() {
 	
 	DijkstraTest();
+	loading2Test();
+	packageTest(); 
+
 	
 	return 0;
 }
